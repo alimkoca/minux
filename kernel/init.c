ba890b104b34df07af10a6b865949d54628536c2
Alim Koca <kinetixcicocuk@gmail.com>
2022-08-06 19:49:52 +0300

    change: init.c, rename header

--- kernel/init.c
+++ kernel/init.c
@@ -1,20 +1,14 @@
 #include <minux/init.h>
-#include <minux/pro.h>
-#include <minux/isr.h>
-#include <minux/virt.h>
+#include <minux/mem.h>
 #include <minux/kernel.h>
 #include <minux/tty.h>
 
 int init_proc(){
-	// Set the GDT table 
+	// Set the GDT, IDT table and enable protected mode
 	set_gdt_tbl();
-	// Set the IDT table for interrupt
 	set_idt_tbl();
-	// Interrupt handler for protected mode
-	hndl_int_rq();
-	// Enable Protected mode
 	en_prot_mode();
-	// Enable virtual memory
+	// Enable virtual memory for every process
 	en_virt_mem();
 	// Finally print success and drop into shell
 	printk("Minux 0.0.1-rc1: Welcome\n");
