c44cba607c564147c983759bb47e0ecbdc5a0d1e
Alim Koca <kinetixcicocuk@gmail.com>
2022-08-11 20:51:48 +0300

    new header: isr.h description: header file for isr.c

---
+++ include/minux/isr.h
@@ -0,0 +1,70 @@
+#ifndef ISR_H
+#define ISR_H
+
+#include <std/type.h>
+
+/*
+ * IDT pointer points
+ * to IDT and using to
+ * load IDT using lidt
+ * instruction
+ */
+struct idt_ptr_mnx {
+	u16 lim;
+	u32 bs;
+} __attribute__((packed));
+
+/*
+ * IDT entry for IDT
+ * table, loading using pointer
+ * and lidt instruction
+ */
+struct idt_entry_mnx {
+	u16 bs_l;
+	u16 ss;
+	u8 res;
+	u8 attr;
+	u16 bs_h;
+} __attribute__((packed));
+
+/*
+ * IDT set table and vector
+ */
+static void set_idt();
+void set_idt_tbl();
+
+extern void int_elem_0();
+extern void int_elem_1();
+extern void int_elem_2();
+extern void int_elem_3();
+extern void int_elem_4();
+extern void int_elem_5();
+extern void int_elem_6();
+extern void int_elem_7();
+extern void int_elem_8();
+extern void int_elem_9();
+extern void int_elem_10();
+extern void int_elem_11();
+extern void int_elem_12();
+extern void int_elem_13();
+extern void int_elem_14();
+extern void int_elem_15();
+extern void int_elem_16();
+extern void int_elem_17();
+extern void int_elem_18();
+extern void int_elem_19();
+extern void int_elem_20();
+extern void int_elem_20();
+extern void int_elem_21();
+extern void int_elem_22();
+extern void int_elem_23();
+extern void int_elem_24();
+extern void int_elem_25();
+extern void int_elem_26();
+extern void int_elem_27();
+extern void int_elem_28();
+extern void int_elem_29();
+extern void int_elem_30();
+extern void int_elem_31();
+
+#endif
