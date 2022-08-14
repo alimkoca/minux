7231fd49dce5fc5751d0c1ddd54b3643d3ff1c48
Alim Koca <kinetixcicocuk@gmail.com>
2022-08-14 13:39:59 +0300

    update header: string.h description: add string and memory functions

--- include/std/string.h
+++ include/std/string.h
@@ -8,9 +8,10 @@
 typedef u16 size_t;
 
 void *memset(void *a, int b, u32 c);
+char *strconv(u32 n, u32 b);
 int memcmp(const void *a, const void *b, size_t n);
+int strcat(const void *a, const void *b);
 size_t strlen(const char *a);
 bool isspace(char a);
-int strcat(const void *a, const void *b);
 
 #endif
