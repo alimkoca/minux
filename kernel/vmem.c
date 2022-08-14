0172395359095f2993b82df8a4d31d62974e27c7
Alim Koca <kinetixcicocuk@gmail.com>
2022-08-14 13:30:10 +0300

    add: vmem.c description: virtual memory functions

---
+++ kernel/vmem.c
@@ -0,0 +1,25 @@
+#include <minux/video.h>
+#include <minux/vmem.h>
+#include <std/type.h>
+
+u32 page_dir_entry_mnx[1024] __attribute__((aligned(4096)));;
+u32 page_tab_entry_mnx[1024] __attribute__((aligned(4096)));;
+
+static void set_page_dir(){
+	for (u32 i = 0; i < 1024; i++){
+		page_dir_entry_mnx[i] = 0x00000002;
+	}
+}
+
+static void set_page_tab(){
+	for (u32 i = 0; i < 1024; i++){
+		page_tab_entry_mnx[i] = (i * 0x1000) | 3;
+	}
+}
+
+void set_page_dtp(){
+	set_page_dir();
+	set_page_tab();
+	page_dir_entry_mnx[0] = ((u32)page_tab_entry_mnx) | 3;
+	en_virt_mem((u32)&page_dir_entry_mnx);
+}
