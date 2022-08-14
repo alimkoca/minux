d8ca9d01d32da645cf613ad7721494ae9c3472c4
Alim Koca <kinetixcicocuk@gmail.com>
2022-08-14 13:33:06 +0300

    update: init.c description: more information about status of system

--- kernel/init.c
+++ kernel/init.c
@@ -1,14 +1,22 @@
 #include <minux/init.h>
 #include <minux/isr.h>
 #include <minux/mem.h>
-//#include <minux/vmem.h>
+#include <minux/vmem.h>
 #include <minux/video.h>
 
 int init_proc(){
 	// Set the GDT, IDT table and enable protected mode
+	printf("minux: loading gdt\n");
 	set_gdt_tbl();
+	printf("minux: loading idt\n");
 	set_idt_tbl();
+	printf("minux: protected mode\n");
 	en_prot_mode();
-	// Finally print success
-	put_str("Minux 0.0.1-rc1: Welcome", 15);
+	// Enable paging and virtual memory
+	printf("minux: paging\n");
+	set_page_dtp();
+
+	for (;;){
+		asm volatile ("hlt");
+	}
 }
